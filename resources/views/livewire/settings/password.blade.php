34dc40f7ec05ae8c42dc84721ea781ff2f333e4f
Restu Edo Setiaji <restuedosetiaji@gmail.com>
2025-03-08 19:06:18 +0700

    Update Viewable Password Field

--- resources/views/livewire/settings/password.blade.php
+++ resources/views/livewire/settings/password.blade.php
@@ -48,6 +48,7 @@ new class extends Component {
                 type="password"
                 required
                 autocomplete="current-password"
+                viewable
             />
             <flux:input
                 wire:model="password"
@@ -55,6 +56,7 @@ new class extends Component {
                 type="password"
                 required
                 autocomplete="new-password"
+                viewable
             />
             <flux:input
                 wire:model="password_confirmation"
@@ -62,6 +64,7 @@ new class extends Component {
                 type="password"
                 required
                 autocomplete="new-password"
+                viewable
             />
 
             <div class="flex items-center gap-4">
